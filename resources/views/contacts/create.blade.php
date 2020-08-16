5be53ba267e5ab8522537c1332acfb33c21a6d34
Daniel Gogov <daniel.gogov@learnchamp.com>
2020-08-16 13:33:36 +0200

    Updated create.blade.php

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -1,26 +1,46 @@
 @extends('base')
 
 @section('content')
-    {!! Form::open([]) !!}
-    <div class="form-group">
-        <table>
-            <tr>
-                <td>{{ Form::label('first_name', 'Firstname') }}</td>
-                <td>{{ Form::text('first_name', '', ['required' => 'required', 'class' => 'form-control']) }}</td>
-            </tr>
-            <tr>
-                <td>{{ Form::label('last_name', 'Lastname') }}</td>
-                <td>{{ Form::text('last_name', '', ['required' => 'required', 'class' => 'form-control']) }}</td>
-            </tr>
-            <tr>
-                <td>{{ Form::label('number', 'Number') }}</td>
-                <td>{{ Form::number('number', '', ['required' => 'required', 'class' => 'form-control']) }}</td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>{{ Form::submit('Create', ['class' => 'btn btn-success']) }}</td>
-            </tr>
-        </table>
+<div class="row">
+    <div class="col-sm-0 col-xl-4"></div>
+
+    <div class="col-sm-12 col-xl-4">
+        {!! Form::open([]) !!}
+        <div class="form-group">
+            <div class="row">
+                <div class="col-2 text-center">
+                    {{ Form::label('first_name', 'Firstname') }}
+                </div>
+                <div class="col">
+                    {{ Form::text('first_name', '', ['required' => 'required', 'class' => 'form-control']) }}
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-2 text-center">
+                    {{ Form::label('last_name', 'Lastname') }}
+                </div>
+                <div class="col">
+                    {{ Form::text('last_name', '', ['required' => 'required', 'class' => 'form-control']) }}
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-2 text-center">
+                    {{ Form::label('number', 'Number') }}
+                </div>
+                <div class="col">
+                    {{ Form::number('number', '', ['required' => 'required', 'class' => 'form-control']) }}
+                </div>
+            </div>
+            
+            <div class="text-center">
+                {{ Form::submit('Create', ['class' => 'btn btn-success mt-1 ml-5']) }}
+            </div>
+        </div>
+        {!! Form::close() !!}
     </div>
-    {!! Form::close() !!}
+
+    <div class="col-sm-0 col-xl-4"></div>
+</div>
 @endsection
